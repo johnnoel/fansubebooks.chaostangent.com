f5b87fdf8ab5d3866a8fa732a8c1a7dc4e905c30
John Noel <chaostangent@gmail.com>
2015-07-21 14:37:06 +0100

    Working Twitter client

--- src/ChaosTangent/FansubEbooks/Twitter/Client.php
+++ src/ChaosTangent/FansubEbooks/Twitter/Client.php
@@ -22,17 +22,17 @@ class Client
     {
         $resolver = new OptionsResolver();
         $this->configureOptions($resolver);
-        $this->options = $resolver->resolve();
+        $this->options = $resolver->resolve($options);
 
         $handlerStack = HandlerStack::create();
         $handlerStack->push(new Oauth1([
             'consumer_key' => $this->options['consumer_key'],
             'consumer_secret' => $this->options['consumer_secret'],
             'token' => $this->options['access_token'],
-            'token_sercet' => $this->options['secret_token'],
+            'token_secret' => $this->options['secret_token'],
         ]));
 
-        $this->client = new Client([
+        $this->client = new GuzzleClient([
             'base_uri' => 'https://api.twitter.com/1.1/',
             'auth' => 'oauth',
             'handler' => $handlerStack,
@@ -54,6 +54,40 @@ class Client
                 'status' => urlencode($status),
                 'trim_user' => 'true',
             ],
+        ]);
+
+        $json = json_decode($resp->getBody(), false);
+        return $json;
+    }
+
+    /**
+     * Get the timeline for a user (or just for the authenticated account)
+     *
+     * @param string $screenName Leave as null for the current user
+     * @param integer $count The number of tweets to get (max 200)
+     * @param string $maxId
+     * @param boolean $includeRetweets
+     * @throws RequestException
+     * @return object The response from the Twitter API
+     * @see https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+     */
+    public function getTimeline($screenName = null, $count = 30, $maxId = null, $includeRetweets = false)
+    {
+        $params = [
+            'count' => $count,
+            'include_rts' => ($includeRetweets) ? 'true' : 'false',
+        ];
+
+        if ($screenName !== null) {
+            $params['screen_name'] = $screenName;
+        }
+
+        if ($maxId !== null) {
+            $params['max_id'] = $maxId;
+        }
+
+        $resp = $this->client->get('statuses/user_timeline.json', [
+            'query' => $params,
         ]);
 
         $json = json_decode($resp->getBody(), false);
