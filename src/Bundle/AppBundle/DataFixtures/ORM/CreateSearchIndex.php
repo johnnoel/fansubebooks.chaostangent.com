112f36adb2d545815b9bdb2be0e6d019a2112459
John Noel <chaostangent@gmail.com>
2015-07-24 19:39:01 +0100

    Data fixture for creating search index

---
+++ src/Bundle/AppBundle/DataFixtures/ORM/CreateSearchIndex.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ChaosTangent\FansubEbooks\Bundle\AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
+
+/**
+ * Create search index
+ *
+ * Because we can't define our own index statements when creating a table, this
+ * kind of fills in the gaps. Kind of janky as it relies on scrying the DB
+ * platform
+ *
+ * @author John Noel <john.noel@chaostangent.com>
+ * @package FansubEbooks
+ */
+class CreateSearchIndex implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        if (!($manager instanceof EntityManager)) {
+            var_dump(get_class($manager));
+            return;
+        }
+
+        $conn = $manager->getConnection();
+        if ($conn->getDatabasePlatform() instanceof PostgreSqlPlatform) {
+            $sql = 'CREATE INDEX lines_idx_ft ON lines USING gin(to_tsvector(\'english\', line))';
+            $conn->executeQuery($sql);
+        } else {
+            var_dump(get_class($conn->getDatabasePlatform()));
+        }
+    }
+}
